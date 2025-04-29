5eb6d376feae207eb09cd7a845f50f466f081402
유현호 <nolja126@gmail.com>
2025-04-29 16:59:23 +0900

    first commit

---
+++ src/main/java/com/example/mentbox/MentboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.mentbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mentbox/MentboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mentbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
